23c3b647c370bdacd311b3d35ed0d765f9951b50
Nick Badger <badg@nickbadger.com>
2014-12-02 18:11:41 -0800

    Much needed nomenclature overhaul and pruning which members belong where.

--- hwiopy/chipsets/cortexA8.py
+++ hwiopy/chipsets/cortexA8.py
@@ -23,16 +23,25 @@ class sitara335(core.soc):
     ''' The sitara 335 SoC. Used in the Beaglebone Black.
     '''
     def __init__(self, mem_filename):
-        # First grab the pinmodes file, which describes which processor pins
+        # First grab the termmodes file, which describes which terminals
         # are capable of which modes, what mem map to look up, etc
-        with open(__path__[0] + '/sitara_pinmodes.json', 'r', newline='') \
-                as json_pinmodes:
-            pin_modes_all = json.load(json_pinmodes)
-        # Call the super() with the parsed pin modes
-        super().__init__(pin_modes_all=pin_modes_all, 
-                mem_map=_cortexA8_mem_map, 
-                registers=_cortexA8_registers, 
-                mem_filename=mem_filename)
+        with open(__path__[0] + '/sitara_termmodes.json', 'r', newline='') \
+                as json_termmodes:
+            terminal_modes = json.load(json_termmodes)
+
+        # Call the super() with the parsed terminal modes
+        super().__init__(terminal_modes=terminal_modes)
+
+        # Grab the filename for the memory mapping
+        self.mem_filename = mem_filename
+
+        # Get the imported definitions of the memory mapping itself and the 
+        # memory registers, both from the datasheets / TRM / etc. The map 
+        # describes the block, ex: the GPIO1 register is at 0x??????, while
+        # the registers describe the bitwise breakup of each type of register,
+        # ex: within the GPIO1 register, the first bit is _, second is _, etc
+        self.mem_mapping = _cortexA8_mem_map
+        self.register_bits = _cortexA8_registers
 
     def __enter__(self):
         ''' Overrides the generic chipset entry method.
@@ -44,35 +53,39 @@ class sitara335(core.soc):
         '''
         self.on_stop()
 
-    def on_start():
+    def on_start(self, *args, **kwargs):
         ''' Must be called to start the device.
         '''
         super().on_start()
         # Need to parse over all pin modes and open mmaps for each
 
-    def on_stop():
+    def on_stop(self, *args, **kwargs):
         ''' Cleans up the started device.
         '''
         super().on_stop()
 
-    def declare_pin():
+    def declare_linked_pin(self, terminal, mode, *args, **kwargs):
         ''' Sets up a pin as something, checks for available modes, etc.
         '''
-        super().declare_pin()
+        # Don't forget to assign the result of the pin declaration
+        pin = super().declare_linked_pin(terminal, mode)
+        # Potentially modify the result of the pin declaration
+        # Now return the pin
+        return pin
 
-    def _setup_pin(self, pin, mode):
-        ''' Gets the pin ready for use. Handles muxing, mode select, etc.
+    def _setup_term(self, term, mode):
+        ''' Gets the terminal ready for use. Handles muxing, mode select, etc.
         '''
-        # Need to figure out the appropriate mmap for the pin
+        # Need to figure out the appropriate mmap for the term
         #    That said, the mmap should already be opened, probably in the 
         #    on_start method. Then go over each mmap that the SoC needs to 
         #    make. This would be after the super() call.
-        # Need to mux the pin
-        # Might need an overlay for the pin
+        # Need to mux the term
+        # Might need an overlay for the term
         pass
 
 
 def create_gpio():
-    ''' Initializes a processor pin as a GPIO pin.
+    ''' Initializes a processor terminal as a GPIO pin.
     '''
     pass
--- hwiopy/core/core.py
+++ hwiopy/core/core.py
@@ -1,16 +1,32 @@
 ''' Core / toplevel members of the hwiopy library. Everything here *should* be 
 platform-independent.
 
+A note on nomenclature: for clarity, I'm referring to anything on an SoC as 
+a terminal, and everything on a device as a pin. I'm going to try to keep this
+division as strict as possible, because I'm confused as balls over here 
+already.
+
+Will probably need to add a virtual terminal at some point? Or maybe the 
+generic SoC defines terminals available in the fallback sysFS mappings? 
+Or summat.
 '''
 
 class device():
     ''' A base object for a generic hardware-inspecific device. Will probably,
     at some point, provide a graceful fallback to sysfs access.
 
-    Something something sooooomething goes here.
+    ### kwargs:
+
+    pinmap:         dict        [pin name] = core.pin object, subclass, etc
+
+    ### Conventional subclass kwargs:
+
+    chipset:        object      core.soc, subclass, etc
     '''
-    def __init__(self, pinmap={}):
+    def __init__(self, pinmap):
+
         self.pinmap = pinmap
+        self.chipset = None
 
     def __enter__(self):
         pass
@@ -22,12 +38,12 @@ class pin():
     ''' A generic single channel for communication on a *device*.
 
     '''
-    def __init__(self, connection, mode, name=None):
+    def __init__(self, terminal, mode, name=None):
         self.name = name
         self.mode = mode
         self.value = None
         # Connection describes the processor pin
-        self.connection = connection
+        self.terminal = terminal
 
     def update(self):
         ''' Update output for output pin, or input for input pin.
@@ -47,76 +63,110 @@ class plug():
 
 class soc():
     ''' A base object for any system-on-a-chip.
-
-    Contains a pinout, which is a dict that takes ['<pinname>'] and yields a
-    dict with keys ['pin', 'name', 'modes']
     '''
-    def __init__(self, pin_modes_all, mem_map={}, registers={}, 
-            mem_filename=None):
-        self.pin_modes_all = pin_modes_all
-        self.mem_map = mem_map
-        self.mem_filename = mem_filename
-        self.registers = registers
+    def __init__(self, terminal_modes):
+        self.terminal_modes = terminal_modes
+        self.running = False
 
-        # Every pin must be defined in pin_modes_available; use keys as pins.
-        self.pins = list(pin_modes_all)
+        # Set the conventions for memory reference dicts, which might not be
+        # used for every SoC (but probably will?)
+        # mem_filename is the file system location of the memory map
+        self.mem_filename = None
+        # mem_mapping describes which registers are where, ex the 4KB GPIO1 
+        # register is at 0x??????
+        self.mem_mapping = None
+        # The register_bits describe which bits correspond to what within each
+        # type of register, ex the gpio config is offset 0x154 and to set the
+        # idle mode you change bits 3-4
+        self.register_bits = None
+
+        # Every terminal must be defined in terminal_modes
+        # The keys in terminal_modes therefore provide the definition for
+        # the terminals
+        self.terminals = list(terminal_modes)
 
         # No keys have been declared to start. This dict is used to actually
-        # setup the pins with on_start is called and clean up with on_stop
-        self.pins_declared = {}
+        # set up the terminals with on_start and clean up with on_stop
+        self.terminals_declared = {}
 
-        # Add any declarable pins to pins_available
-        self.pins_available = {}
-        # Inspect each pin_mode_available to make sure it's declarable
-        for sys_name, sys_pin in self.pin_modes_all.items():
+        # Add any declarable terminals to terminals_available
+        self.terminals_available = {}
+        # Inspect each terminal_mode to make sure it's declarable
+        for sys_name, sys_term in self.terminal_modes.items():
             # Predeclare a lookup flag
-            pin_available = False
-            # Look into the sys_pin dict at each mode defined in 'modes'
-            for mode in sys_pin['modes'].values():
+            terminal_available = False
+            # Look into the sys_term dict at each mode defined in 'modes'
+            for mode in sys_term['modes'].values():
                 # If the 'mode_num' is defined, we can declare it as a mode
                 if mode['mode_num']:
-                    pin_available = True
-            # If the pin is available, declare it as such.
-            if pin_available:
-                self.pins_available[sys_name]
+                    terminal_available = True
+            # If the terminal is available, declare it as such.
+            self.terminals_available[sys_name] = terminal_available
 
-    def declare_pin(self, pin_to_declare, mode):
-        ''' Sets up the pin for on_start initialization and returns a 
+    def declare_linked_pin(self, terminal, mode):
+        ''' Sets up the terminal for on_start initialization and returns a 
         pin object.
-
-        pin_to_declare must be of type soc_pin, and the soc_pin device must be
-        the same object as the pin being declared.
         '''
         # Error traps
         # --------------
 
-        # First make sure pin_modes_available exists and has content.
-        if not pin_modes_all:
-            raise AttributeError('Available pin modes must be specified '
+        # First make sure terminal_modes exists and has content.
+        if not self.terminal_modes:
+            raise AttributeError('Available terminal modes must be specified '
                 'prior to declaring a pin.')
 
-        # Now make sure the pin name is valid:
-        if pin_to_declare not in self.pin_modes_all:
-            raise AttributeError('Invalid pin name.')
+        # Now make sure the terminal name is valid:
+        if terminal not in self.terminal_modes:
+            raise AttributeError('Invalid terminal name.')
 
         # Now check to make sure the mode is valid:
-        if mode not in self.pin_modes_all[pin_to_declare]['modes']:
+        if mode not in self.terminal_modes[terminal]['modes']:
             possible_modes = list(
-                self.pin_modes_all[pin_to_declare]['modes'].values())
-            raise KeyError('Invalid mode type. Current pin may have the '
+                self.terminal_modes[terminal]['modes'].values())
+            raise KeyError('Invalid mode type. Current terminal may have the '
                 'following modes: \n' + possible_modes)
 
-        # Now check to make sure that pin hasn't already been declared
-        if pin_to_declare in self.pins_declared:
-            raise AttributeError('Pin has already been declared. Cannot '
-                're-declare pins after pin initialization.')
+        # Now check to make sure that the terminal hasn't already been used
+        if terminal in self.terminals_declared:
+            raise AttributeError('Terminal has already been declared as a '
+                'pin. Cannot re-declare a terminal after its initialization.')
 
         # Pin declaration
         # ---------------
 
-        self.pins_declared[pin_to_declare] = mode
-        return pin(pin_to_declare, mode)
+        self.terminals_declared[terminal] = mode
+        return pin(terminal, mode)
         # Note that now the chipset must create update, status, etc methods
+
+    def release_terminal(self, terminal):
+        ''' Releases a terminal, allowing re-declaration. Note: the device 
+        must independently release its pin; if it calls the terminal once it's
+        been released, an error will result.
+        '''
+        # Error traps
+        # --------
+
+        # Cannot release a pin connected to an SoC that is currently running
+        if self.running == True:
+            raise RuntimeError('Cannot release a terminal while running.')
+        # Can't release a terminal that hasn't been declared
+        if terminal not in self.terminals_declared:
+            raise KeyError('Cannot release an undeclared terminal.')
+
+        # Meat and bones
+        # ------------
+
+        del self.terminals_declared[terminal]
+        self.terminals_available[terminal] = True
+
+    def mutate_terminal(self, *args, **kwargs):
+        ''' Changes a terminal's function while the device it's attached to is 
+        running. May or may not be overridden for each individual SoC. Only 
+        use if you know what you're doing. Shouldn't be necessary for normal 
+        operations.
+        '''
+        raise NotImplementedError('mutate_terminal must be defined for each '
+            'individual SoC.')
 
     def __enter__(self):
         self.on_start()
@@ -124,18 +174,32 @@ class soc():
     def __exit__(self, type, value, traceback):
         self.on_stop()
 
-    def on_start(self):
+    def on_start(self, *args, **kwargs):
         ''' Must be called to start the device.
         '''
-        for pin, mode in self.pins_declared.items():
-            self._setup_pin(pin, mode)
+        self.running = True
+        for term, mode in self.terminals_declared.items():
+            self._setup_term(term, mode)
 
-    def on_stop(self):
+    def on_stop(self, *args, **kwargs):
         ''' Cleans up the started device.
+        '''
+        for term, mode in self.terminals_declared.items():
+            self._unsetup_term(term, mode)
+        self.running = False
+
+    def _setup_term(self, *args, **kwargs):
+        ''' Gets the terminal ready for use. Handles muxing, mode select, etc. 
+
+        MUST be overridden for each individual SoC that needs to be set up.
         '''
         pass
 
-    def _setup_pin(self, pin, mode):
-        ''' Gets the pin ready for use. Handles muxing, mode select, etc.
+    def _unsetup_term(self, *args, **kwargs):
+        ''' If anything needs to be released before a new call to _setup_term,
+        this is the place to do it.
+
+        MUST be overridden for each individual SoC that needs its terminals to 
+        be released after use.
         '''
         pass
